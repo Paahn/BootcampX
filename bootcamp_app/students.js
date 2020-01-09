c59dc0e1e1d38c617fc1377fcb900dc3938f6e4a
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-09 15:51:59 -0800

    allow a user to specify a cohort name and the limit

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,25 +7,42 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT id, name, cohort_id
-FROM students
-LIMIT 5;
-`)
-.then(res => {
-  console.log(res.rows); // if we logged just res, it would print the whole object
-})
-.catch(err => console.error('query error', err.stack));
 
 pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-LIMIT 5;
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
 `)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-})
-.catch(err => console.error('query error', err.stack));
+}).catch(err => console.error('query error', err.stack));
+
+
+
+
+// pool.query(`
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `)
+// .then(res => {
+//   console.log(res.rows); // if we logged just res, it would print the whole object
+// })
+// .catch(err => console.error('query error', err.stack));
+
+// pool.query(`
+// SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+// FROM students
+// JOIN cohorts ON cohort_id = cohorts.id
+// LIMIT 5;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// })
+// .catch(err => console.error('query error', err.stack));
